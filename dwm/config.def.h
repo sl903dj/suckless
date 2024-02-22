43af0158514acfd5625468fb250726e302a67ea2
Suliang1027 <sl903dj@hotmail.com>
2024-02-22 11:29:32 +0800

    sysbar添加gap

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -2,10 +2,12 @@
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int gappx     = 5;        /* gaps between windows */
+static const unsigned int gappx     = 2;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const int vertpad            = 2;        /* vertical padding of bar */
+static const int sidepad            = 2;        /* horizontal padding of bar */
 static const Bool viewontag         = True;     /* Switch view on tag switch */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayspacing = 2;   /* systray spacing */
