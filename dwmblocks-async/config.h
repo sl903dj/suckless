9719dcc58418a2214cb170c389018579d2a0c519
Suliang1027 <sl903dj@hotmail.com>
2024-03-15 19:21:47 +0800

    add dwmblocks-async

--- dwmblocks-async/config.h
+++ dwmblocks-async/config.h
@@ -2,7 +2,7 @@
 #define CONFIG_H
 
 // String used to delimit block outputs in the status.
-#define DELIMITER "  "
+#define DELIMITER " "
 
 // Maximum number of Unicode characters that a block can output.
 #define MAX_BLOCK_OUTPUT_LENGTH 45
@@ -18,15 +18,13 @@
 
 // Define blocks for the status feed as X(cmd, interval, signal).
 #define BLOCKS(X)         \
-    X("sb-mail", 600, 1)  \
-    X("sb-music", 0, 2)   \
-    X("sb-disk", 1800, 3) \
-    X("sb-memory", 10, 4) \
-    X("sb-loadavg", 5, 5) \
-    X("sb-mic", 0, 6)     \
-    X("sb-record", 0, 7)  \
-    X("sb-volume", 0, 8)  \
-    X("sb-battery", 5, 9) \
-    X("sb-date", 1, 10)
+    X("dwm_memory", 1, 1)  \
+    X("dwm_cpu", 1, 2)   \
+    X("dwm_disk", 1, 3) \
+    X("dwm_network", 500, 4) \
+    X("dwm_volume", 1, 5) \
+    X("dwm_battery", 1, 6)     \
+    X("dwm_date", 1, 7)  \
+    X("dwm_lunar", 1, 8)
 
 #endif  // CONFIG_H
