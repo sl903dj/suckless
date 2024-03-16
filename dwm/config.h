5df4455bf0200ff86c4b633546c7c326cc7de9f8
Suliang1027 <sl903dj@hotmail.com>
2024-03-16 20:32:07 +0800

    add button6

--- dwm/config.h
+++ dwm/config.h
@@ -174,6 +174,7 @@ static const Button buttons[] = {
     { ClkStatusText,        0,              Button3,        sigstatusbar,   {.i = 3} },
     { ClkStatusText,        0,              Button4,        sigstatusbar,   {.i = 4} },
     { ClkStatusText,        0,              Button5,        sigstatusbar,   {.i = 5} },
+    { ClkStatusText,        MODKEY,         Button1,        sigstatusbar,   {.i = 6} },
     { ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
     { ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
     { ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
